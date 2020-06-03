d08b00fc0c03293ec595b7dd3407b42f5052c0d9
sun <jukihabac158@gmail.com>
2020-06-03 07:12:08 +0700

    code Main

---
+++ app/src/main/java/com/example/qlbhcdio/view/ViewRegister.java
@@ -0,0 +1,16 @@
+package com.example.qlbhcdio.view;
+
+public interface ViewRegister {
+    public interface onListenRegister {
+
+        void onId(String s);
+
+        void onPassword(String s);
+
+        void onCPassword(String s);
+
+        void onEmail(String s);
+
+        void onCb(String s);
+    }
+}
